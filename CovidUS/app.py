a0ebfd0c4ec179e4184ed20dd70347929044c2f2
Ahmed Lotfy <lotfyah@yahoo.com>
2020-09-10 19:18:27 -0500

    add updated python app tempaltes and css

--- CovidUS/app.py
+++ CovidUS/app.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify, redirect
 from flask_sqlalchemy import SQLAlchemy
-
 
 app = Flask(__name__)
 
@@ -8,7 +7,7 @@ ENV = 'dev'
 
 if ENV == 'dev':
     app.debug = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///CovidUS'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root123@localhost/CovidUS'
 else:
     app.debug = False
     app.config['SQLALCHEMY_DATABASE_URI'] = ''
@@ -18,19 +17,19 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-class uscases(db.Model):
-    __tablename__ = 'covid_us_county'
+class Covid(db.Model):
+    __tablename__ = 'county_us'
     fips = db.Column(db.Integer, primary_key=True)
     county = db.Column(db.String(200), unique=True)
     state = db.Column(db.String(200))
-    lat = db.Column(db.Integer)
-    long = db.Column(db.Integer)
+    lat = db.Column(db.Float)
+    long = db.Column(db.Float)
     cases = db.Column(db.Integer)
     deaths = db.Column(db.Integer)
     status = db.Column(db.String(200))
     details = db.Column(db.String)
 
-    def __init__(self, county, state, lat, long, cases, deaths):
+    def __county__(self, county, state, lat, long, cases, deaths):
         self.county = county
         self.state = state
         self.cases = cases
@@ -39,19 +38,67 @@ class uscases(db.Model):
         self.long = long
 
 
-@app.route('/')
-def index():
+class CovidDemo(db.Model):
+    __tablename__ = 'demographic_us'
+    fips = db.Column(db.Integer, primary_key=True)
+    county = db.Column(db.String(200), unique=True)
+    state = db.Column(db.String(200))
+    lat = db.Column(db.Float)
+    long = db.Column(db.Float)
+    male = db.Column(db.Integer)
+    female = db.Column(db.Integer)
+    population = db.Column(db.Integer)
+
+    def __demographic__(self, county, state, lat, long, male, female, population):
+        self.county = county
+        self.state = state
+        self.male = male
+        self.female = female
+        self.lat = lat
+        self.long = long
+        self.population = population
+
+
+@app.route('/dashboard')
+def dashboard():
     return render_template('dashboard.html')
 
 
-@app.route('/dataentry', methods=['POST'])
+@app.route('/dataentry', methods=["GET", 'POST'])
 def dataentry():
     if request.method == 'POST':
         state = request.form['state']
         county = request.form['county']
         status = request.form['status']
         details = request.form['deaths']
-        return render_template('dataentry.html', message='new case successfully entered')
+    return render_template('dataentry.html')
+
+
+@app.route('/dbscrape')
+def dbscrape():
+    results = db.session.query(Covid.county, Covid.lat, Covid.long).all()
+
+    hover_text = [result[0] for result in results]
+    lat = [result[1] for result in results]
+    long = [result[2] for result in results]
+
+    covid_data = [{
+        "type": "scattergeo",
+        "locationmode": "USA-states",
+        "lat": lat,
+        "long": long,
+        "text": hover_text,
+        "hoverinfo": "text",
+        "marker": {
+            "size": 15,
+            "line": {
+                "color": "rgb(8,8,8)",
+                "width": 1
+            },
+        }
+    }]
+
+    return jsonify(covid_data)
 
 
 if __name__ == '__main__':
--- CovidUS/static/js/app.js
+++ CovidUS/static/js/app.js
@@ -1,34 +1,82 @@
-function buildPlot() {
-    /* data route */
-  var url = "/dashboard";
+// from data.js 
+var url = "http://127.0.0.1:5000/dbscrape";
   d3.json(url).then(function(response) {
 
     console.log(response);
 
     var data = response;
 
-    var layout = {
-      scope: "usa",
-      title: "COVID-19 CASES PER COUNTY",
-      showlegend: false,
-      height: 600,
-            
-      geo: {
-        scope: "usa",
-        projection: {
-          type: "albers usa"
-        },
-        showland: true,
-        landcolor: "rgb(217, 217, 217)",
-        subunitwidth: 1,
-        countrywidth: 1,
-        subunitcolor: "rgb(255,255,255)",
-        countrycolor: "rgb(255,255,255)"
-      }
-    };
+// get tbody refernce  
+tbody = d3.select('tbody')
 
-    Plotly.newPlot("map", data, layout);
-  });
+
+// use Object.entries to get the data and loop through then insert to html table
+
+function displayData(data) {
+    tbody.text('')
+    data.forEach(function (covidTable) {
+        newRow = tbody.append('tr')
+        Object.entries(covidTable).forEach(function ([key, value]) {
+            newTd = newRow.append('td').text(value)
+        })
+    })
 }
 
-buildPlot();
+displayData(tableData)
+
+//select user input and search button
+var State = d3.select('#select-state')
+var button = d3.select('filter-btn')
+
+
+// search data with date inputs
+function clickSelect() {
+    //don't refresh the page! 
+    d3.event.preventDefault()
+    //print the value that was input 
+    console.log(dateForm.property('value'))
+    //create a new table with the filterd data only    
+     var newTable = tableData.filter(sighting => sighting.datetime === dateForm.property('value')) 
+
+    displayData(newTable)
+}
+
+// event handler
+dateForm.on('change', clickSelect) 
+
+
+
+
+// function buildPlot() {
+    
+//   var url = "http://127.0.0.1:5000/dbscrape";
+//   d3.json(url).then(function(response) {
+
+//     console.log(response);
+
+//     var data = response;
+
+//     var layout = {
+//       scope: "usa",
+//       title: "COVID-19 CASES PER COUNTY",
+//       showlegend: false,
+//       height: 600,    
+//       geo: {
+//         scope: "usa",
+//         projection: {
+//           type: "albers usa"
+//         },
+//         showland: true,
+//         landcolor: "rgb(217, 217, 217)",
+//         subunitwidth: 1,
+//         countrywidth: 1,
+//         subunitcolor: "rgb(255,255,255)",
+//         countrycolor: "rgb(255,255,255)"
+//       }
+//     };
+
+//     Plotly.newPlot("map", data, layout);
+//   });
+// }
+
+// buildPlot();
